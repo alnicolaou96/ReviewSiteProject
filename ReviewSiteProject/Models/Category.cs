ec599af1a2206e5d802e11c489972d65a7fc84d8
alnicolaou96 <alnicolaou96@gmail.com>
2018-07-13 11:36:49 -0400

    first commit

---
+++ ReviewSiteProject/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ReviewSiteProject.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryID { get; set; }
+        public string Brand { get; set; }
+        public virtual ICollection<Review> Review { get; set; }
+    }
+}
---
+++ ReviewSiteProject/Models/Review.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ReviewSiteProject.Models
+{
+    public class Review
+    {
+        [Key]
+        public int ReviewID { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string PublishDate { get; set; }
+
+        [ForeignKey("Category")]
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
